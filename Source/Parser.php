35e48a3efb341b4efe242a8e530bf4dc9f624f68
Anton Moskalenko <moskalenko.anton@gmail.com>
2022-06-28 00:47:17 +0300

    Add parser.

---
+++ Source/Parser.php
@@ -0,0 +1,56 @@
+<?php
+
+namespace Liloi\Stylo;
+
+/**
+ * Stylo parser.
+ */
+class Parser
+{
+    // @todo: add tests
+    private static function parse(array $input): array
+    {
+        // @todo: move parser to separate project [simple-parser]
+        $output = [];
+
+        foreach ($input as $row)
+        {
+            if($row === '---')
+            {
+                $output[] = '<hr/>';
+                continue;
+            }
+
+            $row = preg_replace('/\[(.*?)\]\{(.*?)\}/', "<a href='/wiki/$2'>$1</a>", $row);
+            $row = preg_replace('/\!\[(.*?)\]\((.*?)\)/', "<img src='$2' alt='$1' />", $row);
+            $row = preg_replace('/\[(.*?)\]\((.*?)\)/', "<a href='$2' target='_blank'>$1</a>", $row);
+            $row = preg_replace('/\*\*(.*?)\*\]/', "<b>$1</b>", $row);
+            $row = preg_replace('/__(.*?)__/', "<i>$1</i>", $row);
+            $row = preg_replace('/\%\%(.*?)\%\%/', "<s>$1</s>", $row);
+            $row = preg_replace('/\^\^(.*?)\^\^/', "<u>$1</u>", $row);
+            $row = preg_replace('/^-\[ \](.*)/', "<input type='checkbox' readonly>$1", $row);
+            $row = preg_replace('/^-\[x\](.*)/', "<input type='checkbox' checked='checked' readonly>$1", $row);
+
+            if(isset($row[0]) && $row[0] === '#')
+            {
+                $row = preg_replace('/^#####(.*)/', "<h5>$1</h5>", $row);
+                $row = preg_replace('/^####(.*)/', "<h4>$1</h4>", $row);
+                $row = preg_replace('/^###(.*)/', "<h3>$1</h3>", $row);
+                $row = preg_replace('/^##(.*)/', "<h2>$1</h2>", $row);
+                $row = preg_replace('/^#(.*)/', "<h1>$1</h1>", $row);
+                $output[] = $row;
+                continue;
+            }
+
+            $output[] = '<p>' . $row . '</p>';
+        }
+
+        return $output;
+    }
+
+    // @todo: add tests
+    public static function parseString($input)
+    {
+        return implode("\n", self::parse(explode("\n", $input)));
+    }
+}
