3dd99d9a8dd52af7dd2ac70bb46a56df8b136255
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-04-22 19:18:41 +0300

    [0.2.5] Add stylo result object.

--- Source/StyloResult.php
+++ Source/StyloResult.php
@@ -4,10 +4,21 @@ namespace Liloi\Stylo;
 
 /**
  * Stylo result.
+ *
+ * @todo: Cover by tests
  */
 class StyloResult
 {
-    private ?string $output;
+    private ?string $output = null;
 
+    // @todo [TEST] What magic methods of the PHP you know?
+    public function __construct(string $output)
+    {
+        $this->output = $output;
+    }
 
+    public function getHypertext(): string
+    {
+        return $this->output;
+    }
 }
