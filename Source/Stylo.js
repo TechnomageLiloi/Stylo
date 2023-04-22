65271d2a8763a6dda7160028ac3f6a6a4b569ca9
Anton Moskalenko <moskalenko.anton@gmail.com>
2023-04-22 20:32:54 +0300

    [0.2.9] Update stylo JS object.

--- Source/Stylo.js
+++ Source/Stylo.js
@@ -3,5 +3,25 @@
  * @type {{}}
  */
 const Stylo = {
-    
+    Trigger: {
+        List: [],
+
+        initialize: function ()
+        {
+            $('.stylo .button-trigger').click(function () {
+                const target = $(this);
+                const data = target.data('key');
+                Stylo.Trigger.List.forEach(element => element(data, target));
+            });
+        },
+
+        /**
+         * f(data:string, target:jQuery)
+         */
+        add: function (f)
+        {
+            // @todo: check 'f' is function
+            Stylo.Trigger.List.push(f);
+        }
+    }
 };
